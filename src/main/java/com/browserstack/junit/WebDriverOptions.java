0057b3ec9b6896dfc07691b532f5c54254b394a3
raveerocks <raveendra.tudangil@gmail.com>
2022-01-20 18:39:43 +0530

    Test Runner

--- src/main/java/com/browserstack/junit/WebDriverOptions.java
+++ src/main/java/com/browserstack/junit/WebDriverOptions.java
@@ -18,37 +18,4 @@ public @interface WebDriverOptions {
     boolean rerun() default false;
 
     CucumberOptions cucumberOptions();
-
-   /* boolean dryRun() default false;
-
-    String[] features() default {};
-
-    String[] glue() default {};
-
-    String[] extraGlue() default {};
-
-    String tags() default "";
-
-    String[] plugin() default {};
-
-    boolean publish() default false;
-
-    boolean monochrome() default false;
-
-    String[] name() default {};
-
-    SnippetType snippets() default SnippetType.UNDERSCORE;
-
-
-    boolean useFileNameCompatibleName() default false;
-
-    boolean stepNotifications() default false;
-
-
-    Class<? extends io.cucumber.core.backend.ObjectFactory> objectFactory() default NoObjectFactory.class;
-
-    enum SnippetType {
-        UNDERSCORE, CAMELCASE
-    }*/
-
 }
